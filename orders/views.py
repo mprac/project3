2d08aaa3075e984b32a423e6d47af55cf6f356e0
Majdi Muwanes <majdi.muwanes@gmail.com>
2020-05-03 23:09:45 -0700

    Setup Crispy Forms added forms.py registerform class

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from . forms import RegisterForm
 from django.contrib.auth.models import User
 from django.urls import reverse
 
@@ -13,6 +14,16 @@ def index(request):
         "user": request.user
     }
     return render(request, "orders/main.html")
+
+def join(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect(index)
+    else:
+        form = RegisterForm()
+    return render(request, "orders/join.html", {'form': form})
 
 def logout(request):
     logout(request)
